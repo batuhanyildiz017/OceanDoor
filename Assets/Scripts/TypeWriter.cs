5a294603079949a1e83e02f0a2c99e91f8259491
Kalhklk <yildiz.batuhan017@gmail.com>
2023-07-23 23:01:01 +0300

    started level2 design

--- Assets/Scripts/TypeWriter.cs
+++ Assets/Scripts/TypeWriter.cs
@@ -12,23 +12,40 @@ public class TypeWriter : MonoBehaviour
     AudioSource source; //ses kaynaðý
     string TextOpen; //yazýmýz
     bool okey;  //yazý tamamen girildiðinde klavye sesini durdurmak için deðiþken
+    int gamelevel;
     private void Start()
     {
+        gamelevel = PlayerPrefs.GetInt("GameLevel");
         okey = false;
         source = GetComponent<AudioSource>();
         Text3 = GetComponent<TMP_Text>();
         source.PlayOneShot(typeSound);
-        if (LanguageControl.tr == true) 
-        {
-            TextOpen = "bugun karadenizin 2 kilometre altindaki, simdiye kadar bulunan dunyanin en eski gemisine gidecegiz. " +
-                "arastirmacilar tarafýndan yapilan aciklamada, bulunan geminin 2400 yil oncesine dayanan eski bir yunan ticaret gemisi oldugu soylendi.";
+        if (gamelevel==2) 
+        { 
+            if (LanguageControl.tr == true)
+            {
+                TextOpen = "bugun karadenizin 2 kilometre altindaki, simdiye kadar bulunan dunyanin en eski gemisine gidecegiz. " +
+                    "arastirmacilar tarafýndan yapilan aciklamada, bulunan geminin 2400 yil oncesine dayanan eski bir yunan ticaret gemisi oldugu soylendi.";
+            }
+            else
+            {
+                TextOpen = "today we will go to the oldest ship in the world found so far, 2 kilometers under the black sea." +
+                    "In the statement made by the researchers, it was said that the ship found was an ancient greek merchant ship dating back 2400 years.";
+            }
         }
-        else
+        else if (gamelevel==3)
         {
-            TextOpen = "today we will go to the oldest ship in the world found so far, 2 kilometers under the black sea." +
-                "In the statement made by the researchers, it was said that the ship found was an ancient greek merchant ship dating back 2400 years.";
+            if (LanguageControl.tr == true)
+            {
+                TextOpen = "bu maceramizda mariana cukuruna, dunya uzerinde bilinen en derin noktaya gidecegiz.mariana cukuru buyuk okyanusta, japonya ve endonezya arasýnda yer alýr." +
+                    "yapýlan son olcumlere gore en derin noktasi yaklasik 10.994 metredir.";
+            }
+            else
+            {
+                TextOpen = "in this adventure, we will go to the mariana trench, the deepest known point on Earth. The mariana trench is located in the great ocean, between japan and indonesia." +
+                    " according to the latest measurements, its deepest point is approximately 10,994 meters.";
+            }
         }
-
         StartCoroutine(TypeWrite());
         
     }
--- Assets/Scripts/TypeWriterEnd.cs
+++ Assets/Scripts/TypeWriterEnd.cs
@@ -12,26 +12,46 @@ public class TypeWriterEnd : MonoBehaviour
     AudioSource source;
     string TextOpen;
     bool okey;  //yazý tamamen girildiðinde klavye sesini durdurmak için deðiþken
+    int gamelevel;
     private void Start()
     {
+        gamelevel = PlayerPrefs.GetInt("GameLevel");
         okey = false;
         source = GetComponent<AudioSource>();
         Text3 = GetComponent<TMP_Text>();
         source.PlayOneShot(typeSound);
-        if (LanguageControl.tr==true)
+        if (gamelevel == 3) //target noktasýnja geldiði gibi leveli arttýrdýðý için bir fazla gösteriyoz
+        { 
+            if (LanguageControl.tr == true)
+            {
+                TextOpen = "23 metre uzunlugundaki gemi, " +
+                    "deniz derinligindeki oksijen eksikligi sayesinde neredeyse hic hasar görmemis durumda " +
+                    "arastirmacýlarýndan olusan uluslararasi ekip 3 yýllýk bir ozel gorev icin karadenizin derinliklerinde." +
+                    "su ana dek 60tan fazla gemi enkazi kesfettiler." +
+                    "bir sonraki bolume gecmek icin menuye don ve bir sonraki maceramiz icin denizaltini gelistirmeyi unutma.";
+            }
+            else
+            {
+                TextOpen = "The 23 meter long ship is almost undamaged due to the lack of oxygen in the sea depth." +
+                    "The international team of researchers is in the depths of the black sea for a special mission of 3 years." +
+                    "They have discovered more than 60 shipwrecks so far." +
+                    "Go to the menu to skip to the next section and dont forget to upgrade the submarine for our next adventure.";
+            }
+        }else if (gamelevel == 4) //target noktasýnja geldiði gibi leveli arttýrdýðý için bir fazla gösteriyoz
         {
-            TextOpen = "23 metre uzunlugundaki gemi, " +
-                "deniz derinligindeki oksijen eksikligi sayesinde neredeyse hic hasar görmemis durumda " +
-                "arastirmacýlarýndan olusan uluslararasi ekip 3 yýllýk bir ozel gorev icin karadenizin derinliklerinde."+
-                "su ana dek 60tan fazla gemi enkazi kesfettiler." +
-                "bir sonraki bolume gecmek icin menuye don ve bir sonraki maceramiz icin denizaltini gelistirmeyi unutma.";
-        }
-        else
-        {
-            TextOpen = "The 23 meter long ship is almost undamaged due to the lack of oxygen in the sea depth." +
-                "The international team of researchers is in the depths of the black sea for a special mission of 3 years." +
-                "They have discovered more than 60 shipwrecks so far." +
-                "Go to the menu to skip to the next section and don't forget to upgrade the submarine for our next adventure.";
+            if (LanguageControl.tr == true)
+            {
+                TextOpen = "dip noktasindaki basinc yeryuzundeki basinca gore yaklasik 1000 kat daha fazladýr."+
+                    "mariana cukurunda hayat belirtileri vardir."+
+                    "yapýlan arastirmalar, asiri basincli ve soguk ortamda yasayabilen bircok mikroorganizma, balik ve yengec turunu ortaya cikarmistir."+
+                    "bir sonraki bolume gecmek icin menuye don ve bir sonraki maceramiz icin denizaltini gelistirmeyi unutma.";
+            }
+            else
+            {
+                TextOpen = "the pressure at the bottom is about 1000 times higher than the pressure on the earth. there are signs of life in the mariana trench." +
+                    "studies have revealed many microorganisms, fish and crab species that can live in extreme pressure and cold environments."+
+                    "return to the menu to skip to the next chapter and dont forget to upgrade your sub for our next adventure.";
+            }
         }
         StartCoroutine(TypeWrite());
 
